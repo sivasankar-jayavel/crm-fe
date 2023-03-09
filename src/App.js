05a93e64dba13e6cf5e6d061c818832ba86fb4e4
SIVASANKAR-JAYAVEL <sivasankar6104@gmail.com>
2023-03-09 14:14:48 +0530

    crm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './components/login/Login';
+import Register from './components/login/register';
+
+import ForgetPassword from './components/login/ForgetPassword';
+import Verification from './components/login/Verification';
+import { UserProvider } from './context/UserContext';
+import ChangePassword from './components/login/ChangePassword';
+import { Success } from './components/login/success';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <UserProvider>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/ForgetPassword' element={<ForgetPassword />} />
+          <Route path='/Verification' element={<Verification />} />
+          <Route path='/ChangePassword' element={<ChangePassword />} />
+          <Route path="/success" element={<Success />} />
+        </Routes>
+      </UserProvider>
+
+    </BrowserRouter>
   );
 }
 
 export default App;
+
